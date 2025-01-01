9a3f6993cd290000782d4433672f9d703448c8fd
Conar365 <kvazar382@gmail.com>
2025-01-01 13:27:32 +0300

    unix

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MenuComponent :msg="unixw"/>
+  <MainComponent/>
+  <FouterComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MenuComponent from "./components/MenuComponent.vue"
+import MainComponent from "./components/MainComponent.vue"
+import FouterComponent from "./components/FouterComponent.vue"
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      unixw: "ddqws",
+    };
+  },
   components: {
-    HelloWorld
+    MenuComponent,
+    MainComponent,
+    FouterComponent
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
